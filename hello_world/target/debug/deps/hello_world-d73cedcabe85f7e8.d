63137e99399bab091e585bdda426aa0fb2e1f251
Daniel M <dannielmarinn@gmail.com>
2025-05-23 17:14:44 -0500

    Se creo el proyecto Rust

---
+++ hello_world/target/debug/deps/hello_world-d73cedcabe85f7e8.d
@@ -0,0 +1,5 @@
+/home/daniel/Programming/Rust/hello_world/hello_world/target/debug/deps/hello_world-d73cedcabe85f7e8: src/main.rs
+
+/home/daniel/Programming/Rust/hello_world/hello_world/target/debug/deps/hello_world-d73cedcabe85f7e8.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/daniel/Programming/Rust/hello_world/hello_world/target/debug/hello_world: /home/daniel/Programming/Rust/hello_world/hello_world/src/main.rs
