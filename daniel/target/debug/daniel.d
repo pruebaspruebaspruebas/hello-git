58135f54cc35f4b8ed0909f72b8e5a6a10d676de
Daniel M <dannielmarinn@gmail.com>
2025-05-23 17:20:14 -0500

    Soy Daniel

---
+++ daniel/target/debug/daniel.d
@@ -0,0 +1,1 @@
+/home/daniel/Programming/Rust/hello_world/daniel/target/debug/daniel: /home/daniel/Programming/Rust/hello_world/daniel/src/main.rs
---
+++ daniel/target/debug/deps/daniel-3a2ea7861a34f14c.d
@@ -0,0 +1,5 @@
+/home/daniel/Programming/Rust/hello_world/daniel/target/debug/deps/daniel-3a2ea7861a34f14c: src/main.rs
+
+/home/daniel/Programming/Rust/hello_world/daniel/target/debug/deps/daniel-3a2ea7861a34f14c.d: src/main.rs
+
+src/main.rs:
